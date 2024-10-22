42a7f54a75ddbcbf84ce044064ca3d1586c68130
Garan <luigipeg12345@gmail.com>
2024-10-21 21:04:45 -0600

    Arm mostly finished
    - Can now hit enemies and can be returned to player

--- KickGame/Assets/Scripts/Player/ArmProjectile.cs
+++ KickGame/Assets/Scripts/Player/ArmProjectile.cs
@@ -5,23 +5,40 @@ using UnityEngine;
 public class ArmProjectile : MonoBehaviour
 {
     private CapsuleCollider _trigger;
-    private PlayerArm arm;
+    private PlayerArm _arm;
+    private bool _canBePickedUp = false;
     private void Awake()
     {
+        _arm = GameObject.Find("Player").GetComponent<PlayerArm>();
         _trigger = GetComponentInChildren<CapsuleCollider>();
-        _trigger.enabled = false;
         Invoke("ColliderEnable", 0.5f);
     }
     private void ColliderEnable()
     {
-        _trigger.enabled = true;
+        _canBePickedUp = true;
     }
     private void OnTriggerEnter(Collider other)
     {
-        if (!other.gameObject.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Player") && _canBePickedUp)
+        {
+            Destroy(this.gameObject);
+        }
+        else if (other.gameObject.CompareTag("Enemy"))
+        {
+            Debug.Log("Arm hit enemy!");
+            EnemyAI en = other.GetComponent<EnemyAI>();
+            if (en == null)
+                en = other.GetComponentInParent<EnemyAI>();
+            en.ApplyStun(3f);
+            Destroy(this.gameObject);
+        }
+        else
             return;
-        PlayerArm arm = other.GetComponent<PlayerArm>();
-        arm.HasArm = true;
-        Destroy(this.gameObject);
+        
+    }
+
+    private void OnDestroy()
+    {
+        _arm.HasArm = true;
     }
 }
