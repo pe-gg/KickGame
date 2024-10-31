b70c42b38424c18617f5e4c9e3acb5d94ffc158b
Sean Lynch <seanzolynch@gmail.com>
2024-10-31 11:13:51 -0600

    Sean fixes the proto player

--- KickGame/Assets/Scripts/Player/PlayerAim.cs
+++ KickGame/Assets/Scripts/Player/PlayerAim.cs
@@ -7,11 +7,13 @@ using UnityEngine;
 /// </summary>
 public class PlayerAim : MonoBehaviour
 {
-    [SerializeField] public float aimSensitivity;
+    [SerializeField] private float aimSensitivity;
+    
+    [SerializeField]private float _clamp = 89f;
     [SerializeField] private Transform _camera;
     private Vector2 _mouseDir;
 
-    private float _clamp = 89f;
+
     private float _xRot;
 
     private void Awake()
@@ -22,14 +24,13 @@ public class PlayerAim : MonoBehaviour
     private void LateUpdate() //got lazy and copied this from a youtube tutorial... not ideal but needed to just get SOMETHING done
     {
         transform.Rotate(Vector3.up, _mouseDir.x * Time.deltaTime);
-        _xRot -= _mouseDir.y;
+        _xRot -= _mouseDir.y * Time.deltaTime;
         _xRot = Mathf.Clamp(_xRot, -_clamp, _clamp);
-        Vector3 targetRot = transform.eulerAngles;
-        targetRot.x = _xRot;
-        _camera.eulerAngles = targetRot;
+       // Vector3 targetRot = transform.eulerAngles;//this line takes the euler angle from your transform, so you're taking the player's rotation as well.
+        _camera.localRotation = Quaternion.Euler(_xRot, 0, 0);
     }
     public void HandleMouseInput(Vector2 input)
     {
-        _mouseDir = new Vector2(input.x * aimSensitivity, input.y * (aimSensitivity / 16));
+        _mouseDir = new Vector2(input.x * aimSensitivity, input.y * (aimSensitivity));
     }
 }
