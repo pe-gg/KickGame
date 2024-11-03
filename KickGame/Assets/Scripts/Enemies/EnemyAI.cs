099af78b7c7908dd5a2e1c467fe37951227d833d
Skibbalt <jazw090@gmail.com>
2024-11-03 15:48:24 -0700

    Animations for walk and shoot

--- KickGame/Assets/Scripts/Enemies/EnemyAI.cs
+++ KickGame/Assets/Scripts/Enemies/EnemyAI.cs
@@ -9,6 +9,9 @@ using System.Collections;
 [AddComponentMenu("AI/Enemy AI")]
 public class EnemyAI : MonoBehaviour
 {
+   
+
+
     #region Enemy Stats
 
     [Header("Enemy Stats")]
@@ -123,6 +126,7 @@ public class EnemyAI : MonoBehaviour
     /// </summary>
     private void Start()
     {
+       
         _currentHealth = MaxHealth;
         _currentAmmo = AmmoCapacity;
         _agent = GetComponent<NavMeshAgent>();
@@ -204,6 +208,8 @@ public class EnemyAI : MonoBehaviour
     /// </summary>
     private void Patrol()
     {
+       
+
         if (!_agent.pathPending && _agent.remainingDistance < 0.5f)
         {
             MoveToNextPatrolPoint();
