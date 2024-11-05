b788c198ec896f4d1d7d3235dcdfed4183d2accf
Clara Gavinho <clara.gavinho@gmail.com>
2024-11-05 13:03:22 -0700

    Beginning of game over screen

---
+++ KickGame/Assets/Scripts/UI/GameOverMenu.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverMenu : MonoBehaviour
+{
+    public void Setup()
+    {
+        gameObject.SetActive(true);
+    }
+}
